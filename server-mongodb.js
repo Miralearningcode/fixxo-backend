acf69fb5717b3a9a0efb3a331fcbc545411aa9de
Mira Wänseth <mira.wanseth@utb.ecutbildning.se>
2022-12-22 11:53:24 +0100

    deleted unnecessary comments

--- server-mongodb.js
+++ server-mongodb.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose')
 
+mongoose.set('strictQuery', false)
+
 const initMongoDB = async () => {
     const conn = await mongoose.connect(process.env.MONGODB_URI)
     console.log(`MongoBD is running at ${conn.connection.host}`)
 }
+
+
 
 module.exports = initMongoDB
--- server-webapi.js
+++ server-webapi.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const port = process.env.WEBAPI_PORT || 6000 //Hans har 9999, ha inte 5000 för då har båda portarna 5000 då ser man inte vilken som körs
+const port = process.env.WEBAPI_PORT || 6000 
 const initMongoDB = require('./server-mongodb')
 const cors = require('cors') 
 const bodyParser = require('body-parser') 
@@ -7,16 +7,15 @@ const express = require('express')
 const app = express() 
 
 
-// middleware      system -> middleware -> app
+// middleware   system -> middleware -> app
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))  //Hanterar formulärdata, även lite mer anvancerad forumlärdata t.ex bilder ture eller false spelar ingen roll här
+app.use(express.urlencoded({ extended: true }))  
 app.use(cors()) 
 app.use(bodyParser.json()) 
 
 
 // routes/controllers
 const productsController = require('./controllers/productsController') 
-// const { json } = require('express') ska denna vara med?
 app.use('/api/products', productsController) 
 
 
